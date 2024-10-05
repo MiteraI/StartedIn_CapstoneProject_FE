5eb122f513f99c433c8f859b6dc73ecf08153bed
MiteraI <91677179+MiteraI@users.noreply.github.com>
2024-10-05 22:49:12 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'StartedIn_CapstoneProject_FE',
+  webDir: 'www'
+};
+
+export default config;
