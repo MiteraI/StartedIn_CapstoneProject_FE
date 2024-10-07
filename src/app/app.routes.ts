bb056b5e15f74e601557f6de4ff16f693e9cb559
Minh Pham <tuanminhphamtdn@gmail.com>
2024-10-07 11:26:27 +0700

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,10 @@ export const routes: Routes = [
     loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
   },
   {
+    path: 'login',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: 'https://startedin-api-1300b23c0f01.herokuapp.com/swagger/index.html',
 };
