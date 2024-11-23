ea5626a66e1b6416607d6eb3f2fa585014ec550f
Minh Pham <tuanminhphamtdn@gmail.com>
2024-11-23 15:53:51 +0700

    ms list, task item redes

--- src/app/components/milestone-page/milestone-list/milestone-list-item/milestone-list-item.component.ts
+++ src/app/components/milestone-page/milestone-list/milestone-list-item/milestone-list-item.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'
+import { Component, Input, OnInit } from '@angular/core'
+import { MatIconModule } from '@angular/material/icon'
+import { NzProgressModule } from 'ng-zorro-antd/progress'
+import { Milestone } from 'src/app/shared/models/milestone/milestone.model'
+import { DateDisplayPipe } from 'src/app/shared/pipes/date-display.pipe'
 
 @Component({
   selector: 'app-milestone-list-item',
   templateUrl: './milestone-list-item.component.html',
   styleUrls: ['./milestone-list-item.component.scss'],
+  standalone: true,
+  imports: [DateDisplayPipe, CommonModule, MatIconModule, NzProgressModule]
 })
-export class MilestoneListItemComponent  implements OnInit {
+export class MilestoneListItemComponent implements OnInit {
+  @Input({ required: true }) milestone: Milestone | null = null
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {}
-
 }
