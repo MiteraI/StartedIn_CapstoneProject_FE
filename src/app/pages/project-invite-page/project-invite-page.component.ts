e7a5c201a9669f7b8fd2bbb6446b32cb1168a7e4
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-11-25 03:02:49 +0700

    add notification of joining group

--- src/app/pages/project-invite-page/project-invite-page.component.ts
+++ src/app/pages/project-invite-page/project-invite-page.component.ts
@@ -1,11 +1,10 @@
-import { HttpErrorResponse } from '@angular/common/http'
-import { Component, OnInit } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
-import { catchError } from 'rxjs'
-import { AntdNotificationService } from 'src/app/core/util/antd-notification.service'
-import { RecruitInviteService } from 'src/app/services/recruit-invite.service'
-import { ApplicationType } from 'src/app/shared/enums/application-type.enum'
-import { TeamRole } from 'src/app/shared/enums/team-role.enum'
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AntdNotificationService } from 'src/app/core/util/antd-notification.service';
+import { RecruitInviteService } from 'src/app/services/recruit-invite.service';
+import { ApplicationType } from 'src/app/shared/enums/application-type.enum';
+import { TeamRole } from 'src/app/shared/enums/team-role.enum';
 
 @Component({
   selector: 'app-project-invite-page',
@@ -14,10 +13,15 @@ import { TeamRole } from 'src/app/shared/enums/team-role.enum'
   standalone: true,
 })
 export class ProjectInvitePage implements OnInit {
-  projectId: string = ''
-  role: any
+  projectId: string = '';
+  role: any;
+  projectOverview: any; // Property to store project invite overview data
 
-  constructor(private recruitInviteService: RecruitInviteService, private route: ActivatedRoute, private antdNoti: AntdNotificationService) {}
+  constructor(
+    private recruitInviteService: RecruitInviteService,
+    private route: ActivatedRoute,
+    private antdNoti: AntdNotificationService
+  ) {}
 
   inviteAccept() {
     if (this.role) {
@@ -39,12 +43,19 @@ export class ProjectInvitePage implements OnInit {
   }
 
   ngOnInit() {
-    this.projectId = this.route.snapshot.params['projectId']
-    this.role = TeamRole[this.route.snapshot.params['role'].toUpperCase()]
+    this.projectId = this.route.snapshot.params['projectId'];
+    this.role = TeamRole[this.route.snapshot.params['role'].toUpperCase()];
 
-    this.recruitInviteService.getProjectInviteOverview(this.projectId).subscribe((data) => {
-      //TODO: add data to UI
-      console.log(data)
-    })
+    this.recruitInviteService.getProjectInviteOverview(this.projectId).subscribe({
+      next: (data) => {
+        this.projectOverview = data;
+        console.log('Project Overview:', this.projectOverview);
+      },
+      error: (error: HttpErrorResponse) => {
+        this.antdNoti.openErrorNotification('Lỗi', 'Không thể tải dữ liệu lời mời dự án');
+        console.error('Error fetching project invite overview:', error);
+      },
+    });
   }
 }
+
