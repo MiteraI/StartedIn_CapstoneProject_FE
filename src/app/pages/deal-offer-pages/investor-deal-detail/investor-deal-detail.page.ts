99f1bf54ac68977e98119c7cb06d88198ad828b4
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-11-24 11:49:35 +0700

    redesign for deal detail form

--- src/app/pages/deal-offer-pages/investor-deal-detail/investor-deal-detail.page.ts
+++ src/app/pages/deal-offer-pages/investor-deal-detail/investor-deal-detail.page.ts
@@ -6,6 +6,7 @@ import { NzModalModule } from 'ng-zorro-antd/modal';
 import { DealStatus, DealStatusLabels } from 'src/app/shared/enums/deal-status.enum';
 import { VndCurrencyPipe } from 'src/app/shared/pipes/vnd-currency.pipe';
 import { InvestorDealItem } from 'src/app/shared/models/deal-offer/investor-deal-item.model';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-investor-deal-detail',
@@ -21,11 +22,16 @@ export class InvestorDealDetailPage implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
+    private location: Location // Inject the Location service here
   ) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.deal = data['deal'];
     });
+  }
+
+  goBack(): void {
+    this.location.back(); // Navigate to the previous page
   }
 }
