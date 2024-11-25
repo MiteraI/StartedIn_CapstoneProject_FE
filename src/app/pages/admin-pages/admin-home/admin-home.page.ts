cbdc77ad30ac76f00ef58a24e19d6732104b9423
Minh Pham <tuanminhphamtdn@gmail.com>
2024-11-25 08:34:20 +0700

    basic admin home styling

--- src/app/pages/admin-pages/admin-home/admin-home.page.ts
+++ src/app/pages/admin-pages/admin-home/admin-home.page.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.page.html',
   styleUrls: ['./admin-home.page.scss'],
   standalone: true,
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, MatIconModule]
 })
 export class AdminHomePage implements OnInit {
 
