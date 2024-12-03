ef391d99762d3be686d840f9894eef54f7e8537b
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-12-04 02:15:01 +0700

    add enum for transaction and asset

--- src/app/shared/enums/asset-status.enum.ts
+++ src/app/shared/enums/asset-status.enum.ts
@@ -1,11 +1,13 @@
 export enum AssetStatus {
     AVAILABLE = 1,
     UNAVAILABLE = 2,
-    INMANTAINANCE = 3
+    INMANTAINANCE = 3,
+    SOLD = 4
 }
 
 export const AssetStatusLabels : Record<AssetStatus,string> = {
     [AssetStatus.AVAILABLE]: 'Khả dụng',
     [AssetStatus.UNAVAILABLE]: 'Không khả dụng',
-    [AssetStatus.INMANTAINANCE]: 'Đang bảo trì'
+    [AssetStatus.INMANTAINANCE]: 'Đang bảo trì',
+    [AssetStatus.SOLD]:'Đã thanh lý'
 }
--- src/app/shared/enums/transaction-type.enum.ts
+++ src/app/shared/enums/transaction-type.enum.ts
@@ -1,11 +1,13 @@
 export enum TransactionType {
   DISBURSEMENT = 1,
   ASSET_EXPENSE = 2,
-  OTHER = 3
+  OTHER = 3,
+  ASSET_LIQUIDATION = 4
 }
 
 export const TransactionTypeLabels: Record<TransactionType, string> = {
   [TransactionType.DISBURSEMENT]: 'Giải ngân',
   [TransactionType.ASSET_EXPENSE]: 'Mua tài sản',
-  [TransactionType.OTHER]: 'Giao dịch khác'
+  [TransactionType.OTHER]: 'Giao dịch khác',
+  [TransactionType.ASSET_LIQUIDATION]: 'Thanh lý tài sản',
 }
