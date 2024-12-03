4b1b828700dad8008953506e010ffbc8da9fd239
khoikhuat2204 <khoitrongkhuat2003@gmail.com>
2024-12-03 21:51:49 +0700

    merge from main

--- src/app/components/auth-pages/register-form/register-form.component.ts
+++ src/app/components/auth-pages/register-form/register-form.component.ts
@@ -1,41 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-import { MatInputModule } from '@angular/material/input';
-import {
-  AbstractControl,
-  FormBuilder,
-  FormsModule,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { IonInput, IonButton, IonIcon } from '@ionic/angular/standalone';
-import { MaskitoElementPredicate } from '@maskito/core';
-import { phoneMask } from './mask';
-import { MaskitoDirective } from '@maskito/angular';
-import { addIcons } from 'ionicons';
-import { eyeOffOutline, eyeOutline } from 'ionicons/icons';
-import { RegisterRequest } from 'src/app/shared/models/register.model';
-import { RegisterService } from 'src/app/services/register.service';
-import { catchError, tap, throwError } from 'rxjs';
-import { Router } from '@angular/router';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit } from '@angular/core'
+import { MatInputModule } from '@angular/material/input'
+import { AbstractControl, FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { addIcons } from 'ionicons'
+import { eyeOffOutline, eyeOutline } from 'ionicons/icons'
+import { RegisterRequest } from 'src/app/shared/models/register.model'
+import { RegisterService } from 'src/app/services/register.service'
+import { catchError, tap, throwError } from 'rxjs'
+import { Router } from '@angular/router'
+import { MatSnackBar } from '@angular/material/snack-bar'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { NzSelectModule } from 'ng-zorro-antd/select'
+import { CommonModule } from '@angular/common'
 
 @Component({
   selector: 'app-register-form',
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.scss'],
   standalone: true,
-  imports: [
-    IonIcon,
-    IonButton,
-    IonInput,
-    FormsModule,
-    MatInputModule,
-    ReactiveFormsModule,
-    MaskitoDirective,
-  ],
+  imports: [FormsModule, MatInputModule, ReactiveFormsModule, NzButtonModule, NzCheckboxModule, NzFormModule, NzInputModule, NzSelectModule, CommonModule],
 })
 export class RegisterFormComponent implements OnInit {
-  showPassword = false;
+  showPassword = false
   registerForm = this.formBuilder.group(
     {
       email: ['', Validators.required],
@@ -45,20 +33,12 @@ export class RegisterFormComponent implements OnInit {
       phoneNumber: ['', Validators.required],
     },
     { validator: this.passwordMatchValidator }
-  );
+  )
 
-  readonly phoneMask = phoneMask;
+  
 
-  readonly maskPredicate: MaskitoElementPredicate = async (el) =>
-    (el as HTMLIonInputElement).getInputElement();
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private registerService: RegisterService,
-    private router: Router,
-    private snackBar: MatSnackBar
-  ) {
-    addIcons({ eyeOutline, eyeOffOutline });
+  constructor(private formBuilder: FormBuilder, private registerService: RegisterService, private router: Router, private snackBar: MatSnackBar) {
+    addIcons({ eyeOutline, eyeOffOutline })
   }
 
   ngOnInit() {}
@@ -66,43 +46,36 @@ export class RegisterFormComponent implements OnInit {
   onSubmit() {
     //return if form invalid
     if (this.registerForm.invalid) {
-      return;
+      return
     }
-    const phoneNumber = this.registerForm.value.phoneNumber?.replace(/-/g, '');
-    const registerData: RegisterRequest = this.registerForm.getRawValue();
-    registerData.phoneNumber = phoneNumber;
-
+    const registerData: RegisterRequest = this.registerForm.getRawValue()
     this.registerService
       .register(registerData)
       .pipe(
         tap((response: string) => {
-          this.router.navigate(['login']);
+          this.router.navigate(['login'])
           this.snackBar.open(response, 'Đóng', {
             duration: 5000,
-          });
+          })
         }),
         catchError((error) => {
-          return throwError(error);
+          return throwError(error)
         })
       )
-      .subscribe();
+      .subscribe()
   }
 
   passwordMatchValidator(control: AbstractControl) {
-    const password = control.get('password');
-    const confirmedPassword = control.get('confirmedPassword');
-    if (
-      password &&
-      confirmedPassword &&
-      password.value !== confirmedPassword.value
-    ) {
-      confirmedPassword.setErrors({ passwordMismatch: true });
+    const password = control.get('password')
+    const confirmedPassword = control.get('confirmedPassword')
+    if (password && confirmedPassword && password.value !== confirmedPassword.value) {
+      confirmedPassword.setErrors({ passwordMismatch: true })
     } else {
-      confirmedPassword?.setErrors(null);
+      confirmedPassword?.setErrors(null)
     }
   }
 
   togglePasswordVisibility() {
-    this.showPassword = !this.showPassword;
+    this.showPassword = !this.showPassword
   }
 }
--- src/app/shared/models/register.model.ts
+++ src/app/shared/models/register.model.ts
@@ -1,7 +1,12 @@
 export type RegisterRequest = {
-  email: string;
-  password: string;
-  confirmedPassword: string;
-  fullName: string;
-  phoneNumber: string;
+  email: string
+  password: string
+  confirmedPassword: string
+  fullName: string
+  phoneNumber: string
+  role: string
+  studentCode: string
+  address: string
+  idCardNumber: string
+  academicYear: string
 }
