0718f730a169d9d4ebb8ce0762964a8ab8bf86e2
Minh Pham <tuanminhphamtdn@gmail.com>
2024-12-10 21:04:01 +0700

    group links

--- src/app/layouts/project-side-nav/project-side-nav.component.ts
+++ src/app/layouts/project-side-nav/project-side-nav.component.ts
@@ -3,10 +3,10 @@ import { MatIconModule } from '@angular/material/icon'
 import { MatSidenavModule } from '@angular/material/sidenav'
 import { RouterModule } from '@angular/router'
 import { filter, Subject, takeUntil } from 'rxjs'
-import { AccountService } from '../../core/auth/account.service'
 import { CommonModule } from '@angular/common'
 import { TeamRole } from 'src/app/shared/enums/team-role.enum'
 import { RoleInTeamService } from 'src/app/core/auth/role-in-team.service'
+import { link } from 'ionicons/icons'
 
 @Component({
   selector: 'app-project-side-nav',
@@ -20,10 +20,11 @@ export class ProjectSideNavComponent implements OnInit, OnDestroy {
   @Input() currentId = ''
 
   role: TeamRole | null = null
+  showCall: boolean = false;
   teamRole = TeamRole
   private destroy$ = new Subject<void>()
 
-  constructor(private accountService: AccountService, private roleService: RoleInTeamService) {}
+  constructor(private roleService: RoleInTeamService) {}
 
   ngOnInit(): void {
     this.roleService.role$
@@ -33,7 +34,20 @@ export class ProjectSideNavComponent implements OnInit, OnDestroy {
       )
       .subscribe((role) => {
         this.role = role!.roleInTeam
+        this.showLinksForRole(role!.roleInTeam)
       })
+  }
+
+  showLinksForRole(role: TeamRole) {
+    if (role === TeamRole.LEADER) {
+      this.projectSideNavLinks.find(link => link.linkName === 'tasks')!.hide = false;
+      this.contractSideNavLinks.find(link => link.linkName === 'disbursements')!.hide = false;
+      this.showCall = true;
+    } else if (role === TeamRole.INVESTOR) {
+      this.contractSideNavLinks.find(link => link.linkName === 'investor-disbursements')!.hide = false;
+    } else {
+      this.projectSideNavLinks.find(link => link.linkName === 'tasks')!.hide = false;
+    }
   }
 
   ngOnDestroy() {
@@ -44,6 +58,52 @@ export class ProjectSideNavComponent implements OnInit, OnDestroy {
   sideNavToggle() {
     this.opened = !this.opened
   }
+
+  overviewSideNavLinks: {
+    linkName: string
+    iconName: string
+    linkText: string
+    hide?: boolean
+  }[] = [
+    { linkName: 'dashboard', iconName: 'dashboard', linkText: 'Dashboard' },
+    { linkName: 'charter', iconName: 'info', linkText: 'Tuyên ngôn' },
+  ]
+
+  projectSideNavLinks: {
+    linkName: string
+    iconName: string
+    linkText: string
+    hide?: boolean
+  }[] = [
+    { linkName: 'milestones', iconName: 'flag', linkText: 'Cột Mốc' },
+    { linkName: 'tasks', iconName: 'assignment', linkText: 'Tác Vụ', hide: true },
+    { linkName: 'meeting', iconName: 'insert_invitation', linkText: 'Lịch Hẹn' },
+    { linkName: 'assets', iconName: 'inventory', linkText: 'Tài Sản' },
+    //{ linkName: 'documents', iconName: 'folder', linkText: 'Tài Liệu' },
+  ]
+
+  contractSideNavLinks: {
+    linkName: string
+    iconName: string
+    linkText: string
+    hide?: boolean
+  }[] = [
+    { linkName: 'contracts', iconName: 'history_edu', linkText: 'Hợp Đồng' },
+    { linkName: 'disbursements', iconName: 'account_balance', linkText: 'Giải Ngân', hide: true },
+    { linkName: 'investor-disbursements', iconName: 'account_balance', linkText: 'Giải Ngân', hide: true },
+    { linkName: 'transactions', iconName: 'swap_horiz', linkText: 'Giao Dịch' },
+    { linkName: 'equity', iconName: 'equalizer', linkText: 'Cổ Phần' },
+  ]
+
+  callSideNavLinks: {
+    linkName: string
+    iconName: string
+    linkText: string
+    hide?: boolean
+  }[] = [
+    { linkName: 'investment-call', iconName: 'local_atm', linkText: 'Gọi Vốn' },
+    { linkName: 'recruitment-post', iconName: 'plagiarism', linkText: 'Đăng Tuyển' },
+  ]
 
   sharedSideNavLinks: {
     linkName: string
