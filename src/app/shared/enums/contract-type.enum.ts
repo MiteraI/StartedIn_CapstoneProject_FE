a8e7be6f1ccf89d7eb8ea1c6ae10c20668360304
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-12-13 01:40:26 +0700

    add enum for contract

--- src/app/shared/enums/contract-type.enum.ts
+++ src/app/shared/enums/contract-type.enum.ts
@@ -1,9 +1,11 @@
 export enum ContractType {
   INTERNAL = 1,
-  INVESTMENT = 2
+  INVESTMENT = 2,
+  LIQUIDATIONNOTE = 3
 }
 
 export const ContractTypeLabels: Record<ContractType, string> = {
   [ContractType.INVESTMENT]: 'Đầu tư',
-  [ContractType.INTERNAL]: 'Nội bộ'
+  [ContractType.INTERNAL]: 'Nội bộ',
+  [ContractType.LIQUIDATIONNOTE]: 'Biên bản thanh lý'
 };
