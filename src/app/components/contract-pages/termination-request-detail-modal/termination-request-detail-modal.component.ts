9f39b088de0b42d0bcc455c9b708ca89f8bf5ed1
Minh Pham <tuanminhphamtdn@gmail.com>
2024-12-14 15:44:07 +0700

    termination lists

--- src/app/components/contract-pages/termination-request-detail-modal/termination-request-detail-modal.component.ts
+++ src/app/components/contract-pages/termination-request-detail-modal/termination-request-detail-modal.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NZ_MODAL_DATA, NzModalRef } from 'ng-zorro-antd/modal';
+import { NzNotificationService } from 'ng-zorro-antd/notification';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
-import { AntdNotificationService } from 'src/app/core/util/antd-notification.service';
 import { TerminationRequestService } from 'src/app/services/termination-request.service';
 import { TerminationRequestModel } from 'src/app/shared/models/termination-request/termination-request.model';
 
 interface IModalData {
   projectId: string,
+  isLeader: boolean,
   request: TerminationRequestModel
 }
 
@@ -21,43 +23,53 @@ interface IModalData {
   imports: [
     CommonModule,
     MatIconModule,
-    NzSpinModule
+    NzSpinModule,
+    NzButtonModule
   ]
 })
-export class TerminationRequestDetailModalComponent  implements OnInit {
+export class TerminationRequestDetailModalComponent {
   data: IModalData = inject(NZ_MODAL_DATA);
   isLoading: boolean = false;
 
   constructor(
     private terminationRequestService: TerminationRequestService,
-    private notification: AntdNotificationService,
+    private notification: NzNotificationService,
     private modalRef: NzModalRef,
     private router: Router
   ) { }
 
-  ngOnInit() {
-    this.loadDetails();
-  }
-
-  loadDetails() {
-    this.isLoading = true;
-    this.terminationRequestService.get(this.data.projectId, this.data.request.id).subscribe({
-      next: (response) => {
-        this.data.request = {
-          ...this.data.request,
-          userParties: response.userParties
-        }
-        this.isLoading = false;
-      },
-      error: (error) => {
-        this.notification.openErrorNotification('Lỗi', 'Không thể tải chi tiết yêu cầu');
-        this.isLoading = false;
-      }
-    });
-  }
-
   redirectToUser(userId: string) {
     this.router.navigate(['/users', userId]);
     this.modalRef.close();
+  }
+
+  acceptRequest() {
+    this.isLoading = true;
+    this.terminationRequestService.accept(this.data.projectId, this.data.request.id).subscribe({
+      next: (response) => {
+        this.notification.success('', 'Chấp nhận yêu cầu thành công', { nzDuration: 2000 });
+        this.isLoading = false;
+        this.modalRef.close();
+      },
+      error: (error) => {
+        this.notification.error('Lỗi', 'Chấp nhận yêu cầu thất bại', { nzDuration: 2000 });
+        this.isLoading = false;
+      }
+    })
+  }
+
+  rejectRequest() {
+    this.isLoading = true;
+    this.terminationRequestService.reject(this.data.projectId, this.data.request.id).subscribe({
+      next: (response) => {
+        this.notification.success('', 'Từ chối yêu cầu thành công', { nzDuration: 2000 });
+        this.isLoading = false;
+        this.modalRef.close();
+      },
+      error: (error) => {
+        this.notification.error('Lỗi', 'Từ chối yêu cầu thất bại', { nzDuration: 2000 });
+        this.isLoading = false;
+      }
+    })
   }
 }
