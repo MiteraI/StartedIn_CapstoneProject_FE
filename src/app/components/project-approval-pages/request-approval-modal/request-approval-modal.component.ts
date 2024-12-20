3e3b67e40c899602de833a7cff502dcdd3a004a0
krippykat <89195661+khoikhuat2204@users.noreply.github.com>
2024-12-20 22:43:16 +0700

    send project approval and manual start, stop meeting
    bug: manual stop and delete meeting not working, only start works

---
+++ src/app/components/project-approval-pages/request-approval-modal/request-approval-modal.component.ts
@@ -0,0 +1,112 @@
+import { CommonModule } from '@angular/common'
+import { Component, inject, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms'
+import { NzButtonModule } from 'ng-zorro-antd/button'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { NzMessageService } from 'ng-zorro-antd/message'
+import { NZ_MODAL_DATA, NzModalRef } from 'ng-zorro-antd/modal'
+import { NzSkeletonModule } from 'ng-zorro-antd/skeleton'
+import { NzUploadFile, NzUploadModule } from 'ng-zorro-antd/upload'
+import { ProjectApprovalService } from 'src/app/services/project-approval.service'
+import { ProjectCharterService } from 'src/app/services/project-charter.service'
+import { UserService } from 'src/app/services/user.service'
+import { ProjectCharter } from 'src/app/shared/models/project-charter/project-charter.model'
+import { FullProfile } from 'src/app/shared/models/user/full-profile.model'
+
+@Component({
+  selector: 'app-request-approval-modal',
+  templateUrl: './request-approval-modal.component.html',
+  styleUrls: ['./request-approval-modal.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule, NzIconModule, NzUploadModule, NzButtonModule, NzInputModule, NzSkeletonModule],
+})
+export class RequestApprovalModalComponent implements OnInit {
+  readonly nzModalData = inject(NZ_MODAL_DATA)
+  loading = false
+  uploading = false
+
+  currentUser: FullProfile | undefined
+
+  projectId: string
+  isProjectCharterExist: boolean = false
+  approvalRequestForm!: FormGroup
+  fileList: NzUploadFile[] = []
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private projectApprovalService: ProjectApprovalService,
+    private projectCharterService: ProjectCharterService,
+    private messageService: NzMessageService,
+    private nzModalRef: NzModalRef
+  ) {
+    this.projectId = this.nzModalData.projectId
+  }
+
+  ngOnInit() {
+    this.getProjectCharter()
+
+    this.getCurrentUser()
+
+    this.approvalRequestForm = this.fb.group({
+      reason: ['', [Validators.required, Validators.maxLength(500)]],
+    })
+  }
+
+  getCurrentUser() {
+    this.userService.getFullProfile().subscribe({
+      next: (user) => {
+        this.currentUser = user
+        console.log('Current user:', user)
+      },
+      error: (error) => {
+        console.error('Error fetching current user:', error)
+      },
+    })
+  }
+
+  getProjectCharter(): void {
+    this.loading = true
+    this.projectCharterService.get(this.projectId).subscribe({
+      next: () => {
+        this.isProjectCharterExist = true
+        this.loading = false
+      },
+      error: (error) => {
+        console.error('Error fetching project charter:', error)
+        this.loading = false
+      },
+    })
+  }
+
+  submitRequest(): void {
+    if (this.approvalRequestForm.valid) {
+      this.uploading = true
+
+      const formData = new FormData()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      this.fileList.forEach((file: any) => {
+        formData.append('Documents', file)
+      })
+      this.projectApprovalService.requestApproval(this.projectId, this.currentUser?.id!, formData).subscribe({
+        next: (response) => {
+          console.log(response)
+          this.messageService.success('Yêu cầu phê duyệt đã được gửi')
+          this.uploading = false
+          this.nzModalRef.close()
+        },
+        error: (error) => {
+          console.error('Error requesting approval:', error)
+          this.messageService.error(error.error)
+          this.uploading = false
+        },
+      })
+    }
+  }
+
+  //before upload function
+  beforeUpload = (file: NzUploadFile): boolean => {
+    this.fileList = this.fileList.concat(file)
+    return false
+  }
+}
