e7d4550f07d82a61684e5dd84e161e0a247ffd33
NGUYỄN TRUNG TÍN <113057393+tinnguyentrung962k@users.noreply.github.com>
2024-12-22 03:04:54 +0700

    request register detail

---
+++ src/app/components/project-approval-pages/request-register-detail/request-register-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-request-register-detail',
+  templateUrl: './request-register-detail.component.html',
+  styleUrls: ['./request-register-detail.component.scss'],
+})
+export class RequestRegisterDetailComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
